d3169c60ee09b4997c44e267f40b23f35380de6c
Harum Helmy <harumendhah@gmail.com>
2019-09-03 17:04:21 -0400

    Done.

--- src/containers/PizzaList.js
+++ src/containers/PizzaList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Pizza from '../components/Pizza'
+
 class PizzaList extends Component {
 
   render() {
@@ -15,7 +16,13 @@ class PizzaList extends Component {
         </thead>
         <tbody>
           {
-            //render Pizza here
+            this.props.pizzas.map( 
+              pizza => 
+              <Pizza key={pizza.id} 
+                pizza={pizza} 
+                editPizza={this.props.editPizza}
+              /> 
+            )
           }
         </tbody>
       </table>
