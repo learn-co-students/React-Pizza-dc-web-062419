04269309262c594262cdb48b82daa283c677e705
Michelle Lam <shelly2.lam@gmail.com>
2019-09-04 14:30:27 -0400

    Done.

--- src/components/Pizza.js
+++ src/components/Pizza.js
@@ -1,12 +1,12 @@
 import React from "react"
 
-const Pizza = () => {
+const Pizza = ({pizza: {id, topping, size, vegetarian}, onEdit}) => {
   return(
     <tr>
-      <td>{"Replace Me With Pizza Topping"}</td>
-      <td>{"Replace Me With Pizza Size"}</td>
-      <td>{"Replace Me With Vegatarian"}</td>
-      <td><button type="button" className="btn btn-primary">Edit Pizza</button></td>
+      <td>{topping}</td>
+      <td>{size}</td>
+      <td>{vegetarian ? "vegetarian" : "Not Vegetarian"}</td>
+      <td><button type="button" className="btn btn-primary" onClick={() => onEdit(id, topping, size, vegetarian)}>Edit Pizza</button></td>
     </tr>
   )
 }
